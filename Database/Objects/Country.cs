95459cd1ba64239bf11d1c257b0fce40a13e89fd
Janek <66107554+creeper82@users.noreply.github.com>
2024-03-05 18:09:16 +0100

    Create class for Country objects

---
+++ Database/Objects/Country.cs
@@ -0,0 +1,22 @@
+using System.Text.Json.Serialization;
+
+namespace HolidaysDatabase;
+
+public class Country {
+    [JsonPropertyName("isoCode")]
+    public required string IsoCode {get; set;}
+
+    [JsonPropertyName("name")]
+    public required CountryName[] CountryNames {get; set;}
+
+    [JsonIgnore]
+    public string EnglishName => CountryNames[0].Text;
+}
+
+public class CountryName {
+    [JsonPropertyName("language")]
+    public required string Language {get; set;}
+
+    [JsonPropertyName("text")]
+    public required string Text {get; set;}
+}
