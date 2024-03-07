b29a0ff16f4e5397a55efb888bb7920f246b18ee
Janek <66107554+creeper82@users.noreply.github.com>
2024-03-07 18:23:53 +0100

    Add API method to get holidays

---
+++ Database/API/GetHolidays.cs
@@ -0,0 +1,24 @@
+using System.Text.Json;
+
+namespace HolidaysDatabase;
+
+public partial class API {
+
+    public static async Task<List<Holiday>> GetHolidays(string countryISO, DateOnly dateFrom, DateOnly dateTo) {
+        string dateFromStr = dateFrom.ToString("yyyy-MM-dd");
+        string dateToStr = dateTo.ToString("yyyy-MM-dd");
+
+        var response = await HttpClient.GetAsync(
+        $"https://openholidaysapi.org/PublicHolidays?countryIsoCode={countryISO}&validFrom={dateFromStr}&validTo={dateToStr}"
+        );
+
+        if (response.IsSuccessStatusCode) {
+            var json = await response.Content.ReadAsStringAsync();
+            var deserialized = JsonSerializer.Deserialize<List<Holiday>>(json);
+            return deserialized ?? [];
+        }
+
+        else throw new HttpRequestException(message: $"Failed to connect to API.", null, statusCode: response.StatusCode);
+    }
+}
+
