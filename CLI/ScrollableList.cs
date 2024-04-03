7e7d8ada4b90e3390409985489422f0381ce2e0c
Janek <66107554+creeper82@users.noreply.github.com>
2024-04-03 18:29:23 +0200

    Add documentation for ScrollableList

--- CLI/ScrollableList.cs
+++ CLI/ScrollableList.cs
@@ -1,9 +1,17 @@
 namespace CLI;
 
+/// <summary>
+/// Used for displaying a paginated list you can scroll. Unlike <c>ChoiceList</c>, you can't select any item
+/// here. It is only for visual purpose of scrolling, which you can do with <c>MoveForward</c> (scroll down), and
+/// <c>MoveBackward</c> (scroll up) methods. Then, use <c>PaginatedChoices</c> to get the current state of list.
+/// Set the <c>PaginationCount</c> to determine maximum number of visible elements at one moment.
+/// </summary>
+/// <typeparam name="T">Type of the list elements.</typeparam>
+/// <param name="choices">List elements.</param>
 public class ScrollableList<T>(IEnumerable<T> choices)
 {
     public int Position = 0;
-    public int PaginationCount = 10;
+    public int PaginationCount = 9;
 
     public int MaxAllowedPosition
     {
@@ -22,17 +30,25 @@ public class ScrollableList<T>(IEnumerable<T> choices)
             return Choices.Skip(Position).Take(PaginationCount);
         }
     }
-
+    /// <summary>
+    /// Scrolls the list down.
+    /// </summary>
     public void MoveForward()
     {
         if (Position < MaxAllowedPosition) Position++;
     }
 
+    /// <summary>
+    /// Scrolls the list up.
+    /// </summary>
     public void MoveBackward()
     {
         if (Position > 0) Position--;
     }
 
+    /// <summary>
+    /// Check if the pointer got out of bounds and fix it, if so. It might happen if the list content changes.
+    /// </summary>
     public void CheckOutOfBoundsPointer()
     {
         if (Position > MaxAllowedPosition) Position = MaxAllowedPosition;
