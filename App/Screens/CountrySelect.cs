308678c81d92cad5812d63a4e3b09ed6d477ff65
creeper82 <jan.szuszkiewicz06@gmail.com>
2024-05-16 10:42:53 +0200

    Adjust to the new version of SharpViews Use modern error handling methods

--- App/Screens/CountrySelect.cs
+++ App/Screens/CountrySelect.cs
@@ -3,66 +3,28 @@ namespace UpcomingHolidays;
 using HolidaysDatabase;
 using CLI;
 using SharpViews;
+using static SharpViews.ErrorHandling;
 
 public static partial class App
 {
     public static async Task CountrySelect()
     {
 
-        bool retryConnection = true;
+        bool running = true;
 
-        while (retryConnection)
+        List<Country>? countries = await TryGetOrHandleErrorAsync<HttpErrorHandler, List<Country>>(API.GetCountries, loadingText: true);
+
+        if (countries is not null)
         {
-            Console.Clear();
-            Console.WriteLine("\nLoading...");
-            List<Country> countries = [];
+            ChoiceList<Country> countryChoiceList = new(countries.OrderBy(c => c.EnglishName)) {
+                PaginationCount = 5
+            };
 
-            string? error = null;
-
-            try
+            while (running)
             {
-                countries = await API.GetCountries();
-                if (countries.Count == 0) error = "Server responded with empty data.";
-            }
-
-            catch (Exception e) when (e.InnerException is TimeoutException)
-            {
-                error = "The request has timed out. Check internet connection, or try again if it is slow";
-            }
-
-            catch (HttpRequestException e)
-            {
-                error = "Error communicating with server. Check internet connection.\nMessage: " + e.Message;
-            }
-
-            catch (Exception) { error = "Unrecognized error occured. Sorry"; }
-
-            if (error is null)
-            {
-                retryConnection = false;
-
-                ChoiceList<Country> countryChoiceList = new(countries)
-                {
-                    PaginationCount = 5
-                };
-
-                countryChoiceList.UpdateChoices(countryChoiceList.Choices.OrderBy(c => c.EnglishName));
-
-                bool running = true;
-
-                while (running)
-                {
-                    Screens.CountrySelect(countryChoiceList.PaginatedChoices, countryChoiceList.SelectedIndex, countryChoiceList.PaginationStartIndex);
-                    running = await AppLogic.HandleCountrySelect(countryChoiceList);
-                }
-            }
-
-            else
-            {
-                retryConnection = ErrorScreen("Holidays", error);
+                Screens.CountrySelect(countryChoiceList.PaginatedChoices, countryChoiceList.SelectedIndex, countryChoiceList.PaginationStartIndex);
+                running = await AppLogic.HandleCountrySelect(countryChoiceList);
             }
         }
-
-
     }
 }
--- App/Screens/Holidays.cs
+++ App/Screens/Holidays.cs
@@ -3,80 +3,46 @@ namespace UpcomingHolidays;
 using HolidaysDatabase;
 using CLI;
 using SharpViews;
+using static SharpViews.ErrorHandling;
 
 public static partial class App
 {
     public static async Task Holidays(Country selectedCountry)
     {
+        DateOnly today = DateOnly.FromDateTime(DateTime.Now);
+        DateOnly dateFrom = today;
+        DateOnly dateTo = new(today.Year + 1, 12, 31);
 
-        bool retryConnection = true;
-
-        while (retryConnection)
+        List<Holiday>? holidays = await TryGetOrHandleErrorAsync<HttpErrorHandler, List<Holiday>>(() =>
         {
-            Console.WriteLine("\nLoading...");
+            return API.GetHolidays(selectedCountry.IsoCode, dateFrom, dateTo);
+        }, loadingText: true);
 
-            DateOnly today = DateOnly.FromDateTime(DateTime.Now);
-            DateOnly dateFrom = today;
-            DateOnly dateTo = new(today.Year + 1, 12, 31);
+        if (holidays is not null)
+        {
+            bool running = true;
+            bool useEnglish = false;
 
-            List<Holiday> holidays = [];
-
-            string? error = null;
-
-            try
+            ScrollableList<Holiday> holidaysScrollList = new(holidays)
             {
-                holidays = await API.GetHolidays(selectedCountry.IsoCode, dateFrom, dateTo);
-                if (holidays.Count == 0) error = "Server responded with empty data.";
-            }
+                PaginationCount = 5
+            };
 
-            catch (Exception e) when (e.InnerException is TimeoutException)
+            while (running)
             {
-                error = "The request has timed out. Check internet connection, or try again if it is slow";
+                Screens.Holidays(
+                    holidaysScrollList.PaginatedChoices,
+                    selectedCountry.EnglishName,
+                    useEnglish
+                );
+
+                var handleResult = AppLogic.HandleHolidays();
+
+                if (handleResult is AppLogic.HandleHolidaysResult.SwitchLanguage) useEnglish = !useEnglish;
+                else if (handleResult is AppLogic.HandleHolidaysResult.MoveForward) holidaysScrollList.MoveForward();
+                else if (handleResult is AppLogic.HandleHolidaysResult.MoveBackward) holidaysScrollList.MoveBackward();
+                else if (handleResult is AppLogic.HandleHolidaysResult.Cancel) running = false;
             }
-
-            catch (HttpRequestException e)
-            {
-                error = "Error communicating with server. Check internet connection.\nMessage: " + e.Message;
-            }
-
-            catch (Exception) { error = "Unrecognized error occured. Sorry"; }
-
-            if (error is null)
-            {
-                retryConnection = false;
-                bool running = true;
-                bool useEnglish = false;
-
-                ScrollableList<Holiday> holidaysScrollList = new(holidays)
-                {
-                    PaginationCount = 5
-                };
-
-                while (running)
-                {
-                    Screens.Holidays(
-                        holidaysScrollList.PaginatedChoices,
-                        selectedCountry.EnglishName,
-                        useEnglish
-                    );
-
-                    var handleResult = AppLogic.HandleHolidays();
-
-                    if (handleResult is AppLogic.HandleHolidaysResult.SwitchLanguage) useEnglish = !useEnglish;
-                    else if (handleResult is AppLogic.HandleHolidaysResult.MoveForward) holidaysScrollList.MoveForward();
-                    else if (handleResult is AppLogic.HandleHolidaysResult.MoveBackward) holidaysScrollList.MoveBackward();
-                    else if (handleResult is AppLogic.HandleHolidaysResult.Cancel) running = false;
-                }
-            }
-            else
-            {
-                retryConnection = ErrorScreen("Holidays", error);
-            }
-
-
-
         }
-
-
     }
 }
